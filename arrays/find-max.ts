e2ec73fcc843898a2b87db285aef8927a8bde508
Srush123 <srushtibhari2000@gmail.com>
2026-02-22 16:32:23 +0530

    findMax function

---
+++ arrays/find-max.ts
@@ -0,0 +1,14 @@
+function findMax(nums: number[]): number {
+    if (nums.length === 0) {
+        throe new Error("Array cannot be empty");
+    }
+    
+    let max = nums[0];
+
+    for (let i=1; i < nums.length; i++) {
+        if(nums[i] > max) {
+            max = nums[i];
+        }
+    }
+    return max;
+}
